dd6be9d6ee6271458a78baa27cf80a01357f35ec
bangweizhou <58966808+bangweizhou@users.noreply.github.com>
2020-05-24 22:14:25 +1000

    challenge 64

--- minPathSum/minPathSum.go
+++ minPathSum/minPathSum.go
@@ -1,25 +1,20 @@
 package minPathSum
 
-func MinPathSum(grid [][]int) int {
-	res := make([][]int, len(grid))
-	for i := range res {
-		res[i] = make([]int, len(grid[0]))
-	}
-	res[0][0] = grid[0][0]
+func minPathSum(grid [][]int) int {
 	for i := 1; i < len(grid[0]); i++ {
-		res[0][i] = grid[0][i] + res[0][i - 1]
+		grid[0][i] = grid[0][i] + grid[0][i - 1]
 	}
 
 	for i := 1; i < len(grid); i++ {
-		res[i][0] = res[i - 1][0] + grid[i][0]
+		grid[i][0] = grid[i - 1][0] + grid[i][0]
 		for j := 1; j < len(grid[i]); j++ {
-			if res[i - 1][j] > res[i][j - 1] {
-				res[i][j] = res[i][j - 1] + grid[i][j]
+			if grid[i - 1][j] > grid[i][j - 1] {
+				grid[i][j] = grid[i][j - 1] + grid[i][j]
 			} else {
-				res[i][j] = res[i - 1][j] + grid[i][j]
+				grid[i][j] = grid[i - 1][j] + grid[i][j]
 			}
 		}
 	}
 
-	return res[len(res) - 1][len(res[0]) - 1]
+	return grid[len(grid) - 1][len(grid[0]) - 1]
 }
