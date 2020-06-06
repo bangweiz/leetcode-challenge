8d4121d7dedc8a9cc18ad4b1109a7f192d3deab5
bangweizhou <58966808+bangweizhou@users.noreply.github.com>
2020-06-06 12:53:10 +1000

    Day 05

---
+++ june_challenge/randomPickWithWeight.go
@@ -0,0 +1,35 @@
+package june_challenge
+
+import "math/rand"
+
+type Solution struct {
+	p []int
+}
+
+
+func Constructor(w []int) Solution {
+	sum, p := 0, make([]int, len(w))
+	for i, v := range w {
+		sum += v
+		p[i] = sum
+	}
+
+	return Solution{p}
+}
+
+
+func (s *Solution) PickIndex() int {
+	n := rand.Intn(s.p[len(s.p) - 1])
+	left, right := 0, len(s.p) - 1
+	for left <= right {
+		mid := (left + right) / 2
+		if s.p[mid] == n {
+			return mid + 1
+		} else if s.p[mid] > n {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
