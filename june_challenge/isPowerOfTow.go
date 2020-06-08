ee92591ad5c8c1bb16e074e8573ed020716e4ce9
bangweizhou <58966808+bangweizhou@users.noreply.github.com>
2020-06-08 21:22:38 +1000

    day 8

---
+++ june_challenge/isPowerOfTow.go
@@ -0,0 +1,14 @@
+package june_challenge
+
+func isPowerOfTwo(n int) bool {
+	if n <= 0 {
+		return false
+	}
+	for n != 1 {
+		if n % 2 != 0 {
+			return false
+		}
+		n = n / 2
+	}
+	return true
+}
